e041ee1e066248cabba6b1b206582c697b5661f7
Markov Artem <113513575+Ymaster005@users.noreply.github.com>
2023-01-29 17:48:17 +0200

    Add files via upload

---
+++ Lab3/HIT/HIT/HIT.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <locale>
+#include "Windows.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	double x, y, R, x0, y0;
+	x0 = 3;
+	y0 = 4;
+	R = 5;
+	std::cout << "На плоскости лежит окружность с центром в точке O(" << x0 << ", " << y0 << ").Радиус окружности равен " << R;
+	std::cout << "\n" << "Введите координаты точки А" << "\n" << "x = ";
+	std::cin >> x;
+	std::cout << "y = ";
+	std::cin >> y;
+
+	if ((x - x0) * (x - x0) + (y- y0) * (y - y0) <= R * R)
+	{
+		std::cout << "Точка A(" << x << ", " << y << ") попадает в круг с центром в точке O(" << x0 << ", " << y0 << ") радиуса " << R;
+	}
+	
+	else
+	{
+		std::cout << "Точка A(" << x << ", " << y << ") не попадает в круг с центром в точке O(" << x0 << ", " << y0 << ") радиуса " << R;
+	}
+
+	return 0;
+	system("PAUSE");
+
+}
---
+++ Lab3/Max0/Max0/Max0.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <locale>
+#include "Windows.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	int A, B;
+	std::cout << "Введите целое значение А ";
+	std::cin >> A;
+	std::cout << "Введите целое значение В ";
+	std::cin >> B;
+	std::cout << "A = " << A << "\t" << "B = " << B << "\n";
+
+	if (A == B)
+	{
+		std::cout << "Значение А должно отличаться от значения В";
+	}
+
+	else
+	{
+		if (A > B)
+		{
+			std::cout << "Max (" << A << ", " << B << ") = " << A;
+		}
+
+		else
+		{
+			std::cout << "Max (" << A << ", " << B << ") = " << B;
+		}
+	}
+
+	return 0;
+	system("PAUSE");
+
+}
