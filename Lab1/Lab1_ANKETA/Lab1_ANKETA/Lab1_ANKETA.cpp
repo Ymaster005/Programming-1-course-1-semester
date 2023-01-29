f7c935bb6568b235130690b446c167a8a5f1b7bb
Markov Artem <113513575+Ymaster005@users.noreply.github.com>
2023-01-29 17:40:56 +0200

    Add files via upload

---
+++ Lab1/Lab1_ANKETA/Lab1_ANKETA/Lab1_ANKETA.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <locale>
+#include "Windows.h"
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ukr");
+    std::wcout << L"Прізвище : Марков\nІм'я : Артем\nПо батькові : Олександрович\nСтать : чоловіча\nДата народження : 2005-09-28\nАдреса : Dnipro, Ukraine\nНомер телефону : +380 66 707 8587\nМісце навчання : ФПМ, ДНУ ім. Олеся Гончара\nГрупа : ПА-22-1\nХоббі : комп'ютернi iгри\n";
+}
---
+++ Lab1/Lab1_SQUARE/Lab1_SQUARE/Lab1_SQUARE.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "QQQQQQQQQQQ\nQ         Q\nQ         Q\nQ         Q\nQ         Q\nQQQQQQQQQQQ";
+}
